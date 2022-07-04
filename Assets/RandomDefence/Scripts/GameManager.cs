40be1f02c2746e9330142bd7dfed76f0094e112d
함승호 <hamsh2003@naver.com>
2022-07-04 22:37:09 +0900

    클릭 기능 구현 시작

--- Assets/RandomDefence/Scripts/GameManager.cs
+++ Assets/RandomDefence/Scripts/GameManager.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+
 public class GameManager : MonoBehaviour
 {
     System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-
     private static GameManager instance;
-
     public static GameManager Instance
     { 
         get
@@ -18,6 +18,12 @@ public class GameManager : MonoBehaviour
             return instance;
         }
     }
+
+
+    public Camera camera;
+    private RaycastHit hitData;
+    private Vector3 hitPosition;
+    private float hitDistance;
     private void Awake()
     {
         if(instance == null)
@@ -30,6 +36,8 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+        camera = Camera.main;
     }
 
     void Update()
@@ -41,6 +49,22 @@ public class GameManager : MonoBehaviour
             MonsterPool.GetMonster();
             sw.Stop();
             Debug.Log("<color=aqua> 오브젝트 생성에 걸린 시간 </color>: " + "<color=red>" + sw.ElapsedMilliseconds.ToString() + "</color> ms");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            MouseInput_Movement();
+        }
+    }
+    
+    void MouseInput_Movement()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        hitPosition = hitData.point;
+        hitDistance = hitData.distance;
+        if (Physics.Raycast(ray, out hitData))
+        {
+            Debug.Log(hitData.transform.name);
         }
     }
 }
