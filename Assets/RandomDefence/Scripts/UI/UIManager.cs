0e0ccaa2f3a711152304d367156d28cd4367821b
Seungho <hamsh2003@xosoft.co.kr>
2022-07-13 17:43:42 +0900

    InputManager 추가, InCompanySeungho 연결

--- Assets/RandomDefence/Scripts/UI/UIManager.cs
+++ Assets/RandomDefence/Scripts/UI/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,19 @@ public class UIManager : MonoBehaviour
 {
     public Button btn_Create;
     // Start is called before the first frame update
+    
+    
+    // About HP
+    public Text text_Hp;
+    [SerializeField] private int _Hp;
+    private int _maxHp;
+    private void Awake()
+    {
+         btn_Create.onClick.AddListener(btnFunc_Create);
+         _Hp = _maxHp;
+         text_Hp.text = _Hp + "/" + _maxHp;
+    }
+
     void Start()
     {
         
@@ -17,4 +31,33 @@ public class UIManager : MonoBehaviour
     {
         
     }
+
+    /// <summary>
+    /// 체력에 변경사항이 있을 경우 호출됨.
+    /// </summary>
+    /// <param name="_value"></param>
+    public void ChangeHp(int _value)
+    {
+        _Hp += _value;
+        CheckHp();
+    }
+
+    /// <summary> 체력을 검사하여 죽었는지 확인 </summary>
+    void CheckHp()
+    {
+        text_Hp.text = _Hp + "/" + _maxHp;
+        if (_Hp > 0)
+        {
+            // 살아있다.
+        }
+        else
+        {
+            // 죽음.
+        }
+    }
+    void btnFunc_Create()
+    {
+        MonsterPool.GetMonster();
+    }
+    
 }
