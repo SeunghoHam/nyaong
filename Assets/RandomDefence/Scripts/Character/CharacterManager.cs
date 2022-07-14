6282e0262ebd26f607e656521234a316c49be31a
Seungho <hamsh2003@xosoft.co.kr>
2022-07-14 10:58:32 +0900

    캐릭터 랜덤 생성 확률 시트 제작

---
+++ Assets/RandomDefence/Scripts/Character/CharacterManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab_Character1;
+    [SerializeField] private GameObject prefab_Character2;
+
+
+    private int _SpawnObjectRange;
+    private float _probability;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void RandomCreate()
+    {
+        
+        //switch ()
+        {
+            
+        }   
+    }
+    void RandomCreate_Normal()
+    {
+        
+    }
+
+    void RandomCreate_Rare()
+    {
+        
+    }
+
+    void RandomCreate_Epic()
+    {
+        
+    }
+
+    void RandomCreate_Legend()
+    {
+        
+    }
+}
